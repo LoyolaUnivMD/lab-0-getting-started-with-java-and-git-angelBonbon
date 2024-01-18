0a95a272ff029466aab63c7233ee8a8310ef9cd6
Lab0_BonitaRodrigues <brodrigues@loyola.edu>
2024-01-18 15:30:35 -0500

    Created the intials of the progammer (me) using ASCII art.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,22 @@
 //HelloWorld.java
 
+// Programmers: Bonita Rodrigues
+// Course:  CS 212, Dr.Nweke
+// Due Date: 1/19/24
+// Lab Assignment: 0
+// Problem Statement:  Output programmers’ initial
+// Input: None; there is no user input in this program
+// Output:  The initials of the programmers in ASCII art form
+// Credits: none
+
 class HelloWorld {
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
+        System.out.println(" ___     ___");
+        System.out.println("| +  \\  | +  \\");
+        System.out.println("|    /  |    /");
+        System.out.println("| +  \\  | | \\");
+        System.out.println("|____/  |_|\\_\\ ");
         
     }
 }
